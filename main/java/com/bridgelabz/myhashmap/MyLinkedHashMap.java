d1c43fc77f0cb8d96ce0ca7c7466edd8aa89b93d
Shubham Rana <ranashubham7296@gmail.com>
2020-11-05 17:26:52 +0530

    Added: Ability to find frequency of words in large paragraphs

---
+++ main/java/com/bridgelabz/myhashmap/MyLinkedHashMap.java
@@ -0,0 +1,102 @@
+package com.bridgelabz.myhashmap;
+
+import java.util.ArrayList;
+
+import com.bridgelabz.mylinkedlistjava.MyLinkedList;
+import com.bridgelabz.mylinkedlistjava.NodeNotFoundException;
+
+public class MyLinkedHashMap<K, V>
+{
+	private final int numberOfBuckets;
+	ArrayList <MyLinkedList<K>> myBucketArray;
+
+	public MyLinkedHashMap()
+	{
+		this.numberOfBuckets = 10;
+		this.myBucketArray = new ArrayList<>(numberOfBuckets);
+		
+		//Initializing empty buckets in bucket array
+		for (int i = 0; i < numberOfBuckets; i++)
+		{
+			this.myBucketArray.add(null);
+		}
+	}
+	
+	/**
+	 * Calculates and returns the index of bucket array for a node based on the hashCode of it's key.
+	 * @param key
+	 * @return
+	 */
+	private int getBucketIndex(K key) 
+	{
+		if(key == null)
+			return (Integer) null;
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % this.numberOfBuckets;
+		return index;
+	}
+	
+	/**
+	 * @return Number of indices in the bucket array list.
+	 */
+	public int getNumberOfBuckets() 
+	{
+		return this.numberOfBuckets;
+	}
+	
+	public V get(K key) 
+	{
+		int index = this.getBucketIndex(key);
+		MyLinkedList <K> myLinkedList = this.myBucketArray.get(index);
+		if(myLinkedList == null)
+			return null;
+		
+		try {
+			MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.findNodeWithKey(key);
+			return myMapNode.getValue();
+		} catch (NodeNotFoundException e) 
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Adds a map node at the bottom of a MapNode List. If bucket that should contain the Map node list is empty
+	 * then a new empty MapNode list will be created in that bucket and then Map node is added to it. Idea behind choosing
+	 * bucket for a node is that the remainder of the division of hashCode of node's key with 10 will give number from 0 to 9.
+	 * And this number will be chosen to pick one of the index in ArrayList of buckets. If the chosen bucket(index) is empty then
+	 * we initialize a new empty Node list of type MyLinkedList and add the node list in that bucket to populate it with it's
+	 * first element. If the bucket is not empty and already contains some linked node list then we add our new map node at
+	 * the last of this node list.
+	 * @param key index of the bucket
+	 * @param value
+	 */
+	public void add(K key, V value) 
+	{
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		
+		if(myLinkedList == null)
+		{
+			myLinkedList = new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedList);
+		}
+		
+		try {
+			MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.findNodeWithKey(key);
+			myMapNode.setValue(value);
+		} catch (NodeNotFoundException e) {
+			MyMapNode<K, V> myMapNode = new MyMapNode<>(key, value);
+			myLinkedList.addBottom(myMapNode);
+		}
+	}
+	
+	/**
+	 *myBucketArray is of type array list, and contents of it is of type myLinkedList, which has it's own toString() defined.
+	 *Hence to convert this to array, MyLinkedList's toString() method is called.
+	 */
+	public String toString()
+	{
+		return "MyLinkedHashMap list{"+myBucketArray+"}";
+	}
+}
---
+++ test/java/com/bridgelabz/myhashmaptest/MyLinkedHashMapTest.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.myhashmaptest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.myhashmap.MyLinkedHashMap;
+
+public class MyLinkedHashMapTest 
+{
+	@Test
+	public void givenASenetenceWtihWordParanoid_ShouldreturnParaniodFrequency()
+	{
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberatily into "
+				+ "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		
+		String [] words = sentence.toLowerCase().split(" ");
+		
+		//Below creating node of type MyLinkedHashMap and adding it into one of the bucket's linked node list.
+		//Bucket is chosen based on hashCode of key object and in that bucket's linked list, corresponding 
+		//node is found based on key. value of node is incremented if the node is already present in that bucket. 
+		for (String word: words)
+		{
+			Integer value = myLinkedHashMap.get(word);
+			if( value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		
+		System.out.println(myLinkedHashMap);
+		int frequency = myLinkedHashMap.get("paranoid");
+		Assert.assertEquals(3, frequency);
+	}
+}
